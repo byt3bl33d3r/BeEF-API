cc9702a0a9b90806157dcc5b47c433d6cb4ccaea
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-06-09 10:46:22 +0200

    initial setup for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'BeEF-API',
+  packages = ['BeEF-API'],
+  version = '0.1',
+  description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
+  author = 'byt3bl33d3r',
+  author_email = 'byt3bl33d3r@gmail.com',
+  url = 'https://github.com/byt3bl33d3r/BeEF-API',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = ['api', 'beef', 'BeEF'],
+  classifiers = [],
+)
