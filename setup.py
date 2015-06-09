bcb907189123875398e3b72235cca7a5937fa7c0
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-06-09 12:05:45 +0200

    updated .gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'beefapi',
-  version = '0.1',
+  version = '0.1a',
   description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
   author = 'byt3bl33d3r',
   author_email = 'byt3bl33d3r@gmail.com',
