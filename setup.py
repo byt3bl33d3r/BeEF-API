4d6603ad8f160d8030ddd83a8a37123ecd534b39
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-06-09 11:47:33 +0200

    pypi noob , sue me

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from distutils.core import setup
 
 setup(
   name = 'beefapi',
-  packages = ['beefapi'],
-  version = '0.1',
+  version = '0.2',
   description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
   author = 'byt3bl33d3r',
   author_email = 'byt3bl33d3r@gmail.com',
   url = 'https://github.com/byt3bl33d3r/BeEF-API',
-  download_url = 'https://github.com/byt3bl33d3r/BeEF-API/tarball/0.1',
   keywords = ['api', 'beef', 'BeEF'],
-  classifiers = [],
+  py_modules=['beefapi'],
 )
