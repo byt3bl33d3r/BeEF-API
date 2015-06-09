514f699d99b469ceca94c967fed6f86b0bb24962
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-06-09 10:53:32 +0200

    added an init file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-  name = 'BeEF-API',
-  packages = ['BeEF-API'],
+  name = 'beefapi',
+  packages = ['beefapi'],
   version = '0.1',
   description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
   author = 'byt3bl33d3r',
