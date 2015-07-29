eb8157e45819a51975a9cb47ae6df652ad1f08dc
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-07-29 23:45:33 +0200

    converted setup script to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-  name = 'beefapi',
-  version = '0.1',
-  description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
-  author = 'byt3bl33d3r',
-  author_email = 'byt3bl33d3r@gmail.com',
-  url = 'https://github.com/byt3bl33d3r/BeEF-API',
-  keywords = ['api', 'beef', 'BeEF'],
-  py_modules=['beefapi'],
+    name='beefapi',
+    version='0.2',
+    description = 'Python library that facilitates interfacing with BeEF via it\'s RESTful API',
+    url='https://github.com/byt3bl33d3r/BeEF-API',
+    author='byt3bl33d3r',
+    author_email='byt3bl33d3r@gmail.com',
+    license='GNU',
+
+    classifiers=[
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    keywords='beef BeEF API',
+    packages=find_packages(),
+    install_requires=['requests'],
+    include_package_data=False,
 )
